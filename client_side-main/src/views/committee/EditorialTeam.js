a3c22a890e0fb05bfbe1537a9497a0493702d575
Haritha Himansha <143889589+hhimansha@users.noreply.github.com>
2024-10-01 19:07:28 +0530

    Update past committee section

--- client_side-main/src/views/committee/EditorialTeam.js
+++ client_side-main/src/views/committee/EditorialTeam.js
@@ -85,109 +85,6 @@ const EditorialTeam = () => {
 
         
 
-
-        <br />
-        <div className="editorialteamframe1">
-          <div className="framegroup">
-            <h3 className="editorial-team">Past Committee 21/22</h3>
-          </div>
-        </div>
-        <div className="lorem-ipsum-dolor-container1">
-          <b>Lorem ipsum dolor sit amet</b>
-          <span>
-            , consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-            labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-            exercitation ullamco laboris nisi ut aliquip ex ea commodo
-            consequat. Duis aute irure dolor in reprehenderit in voluptate velit
-            esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-            cupidatat non proident, sunt in culpa qui officia deserunt mollit
-            anim id est laborum.
-          </span>
-        </div>
-        <div className="publicity-frame">
-          <div className="lead-pic-member">
-            <div className="anjula-member">
-              <div className="anjula-member-child" />
-              <img
-                className="lead-pic-icon"
-                loading="eager"
-                alt=""
-                src={KavinduPerera}
-              />
-            </div>
-            <b className="abiru-abayapala-leader-container">
-              <p className="abiru-abayapala">Kavindu Perera</p>
-              <p className="leader">Chairperson</p>
-            </b>
-          </div>
-          <div className="lead-pic-member1">
-            <div className="frame-div">
-              <div className="frame-child1" />
-              <img
-                className="member-icon"
-                loading="eager"
-                alt=""
-                src={WithushanPhilip}
-              />
-            </div>
-            <b className="chamindu-dilneth-member-container">
-              <p className="chamindu-dilneth">Withushan Philip</p>
-              <p className="member">Coordinator</p>
-            </b>
-          </div>
-          <div className="lead-pic-member1">
-            <div className="frame-div">
-              <div className="frame-child1" />
-              <img
-                className="member-icon"
-                loading="eager"
-                alt=""
-                src={GangulalWijekoon}
-              />
-            </div>
-            <b className="chamindu-dilneth-member-container">
-              <p className="abiru-abayapala">Gangulal Wijekoon</p>
-              <p className="member">Secretary</p>
-            </b>
-          </div>
-          <div className="lead-pic-member1">
-            <div className="frame-div">
-              <div className="frame-child1" />
-              <img
-                className="member-icon"
-                loading="eager"
-                alt=""
-                src={KishenDeemud}
-              />
-            </div>
-            <b className="chamindu-dilneth-member-container">
-              <p className="chamindu-dilneth">Kishen Deemud</p>
-              <p className="member">Committee Member</p>
-            </b>
-          </div>
-          <div className="lead-pic-member2">
-            <div className="rectangle-parent1">
-              <div className="frame-child2" />
-              <img
-                className="member-icon1"
-                loading="eager"
-                alt=""
-                src={ThanutiPiyarathne}            />
-            </div>
-            <b className="dasith-sathpura-member-container">
-              <p className="dasith-sathpura">Thanuti Piyarathne</p>
-              <p className="member1">Committee Member</p>
-            </b>
-          </div>
-        </div>
-
-
-
-
-
-
-
-
         <br />
         <div className="editorialteamframe1">
           <div className="framegroup">
@@ -282,7 +179,102 @@ const EditorialTeam = () => {
             </b>
           </div>
         </div>
+        <br />
 
+
+        <div className="editorialteamframe1">
+          <div className="framegroup">
+            <h3 className="editorial-team">Past Committee 21/22</h3>
+          </div>
+        </div>
+        <div className="lorem-ipsum-dolor-container1">
+          <b>Lorem ipsum dolor sit amet</b>
+          <span>
+            , consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+            labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+            exercitation ullamco laboris nisi ut aliquip ex ea commodo
+            consequat. Duis aute irure dolor in reprehenderit in voluptate velit
+            esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+            cupidatat non proident, sunt in culpa qui officia deserunt mollit
+            anim id est laborum.
+          </span>
+        </div>
+        <div className="publicity-frame">
+          <div className="lead-pic-member">
+            <div className="anjula-member">
+              <div className="anjula-member-child" />
+              <img
+                className="lead-pic-icon"
+                loading="eager"
+                alt=""
+                src={KavinduPerera}
+              />
+            </div>
+            <b className="abiru-abayapala-leader-container">
+              <p className="abiru-abayapala">Kavindu Perera</p>
+              <p className="leader">Chairperson</p>
+            </b>
+          </div>
+          <div className="lead-pic-member1">
+            <div className="frame-div">
+              <div className="frame-child1" />
+              <img
+                className="member-icon"
+                loading="eager"
+                alt=""
+                src={WithushanPhilip}
+              />
+            </div>
+            <b className="chamindu-dilneth-member-container">
+              <p className="chamindu-dilneth">Withushan Philip</p>
+              <p className="member">Coordinator</p>
+            </b>
+          </div>
+          <div className="lead-pic-member1">
+            <div className="frame-div">
+              <div className="frame-child1" />
+              <img
+                className="member-icon"
+                loading="eager"
+                alt=""
+                src={GangulalWijekoon}
+              />
+            </div>
+            <b className="chamindu-dilneth-member-container">
+              <p className="abiru-abayapala">Gangulal Wijekoon</p>
+              <p className="member">Secretary</p>
+            </b>
+          </div>
+          <div className="lead-pic-member1">
+            <div className="frame-div">
+              <div className="frame-child1" />
+              <img
+                className="member-icon"
+                loading="eager"
+                alt=""
+                src={KishenDeemud}
+              />
+            </div>
+            <b className="chamindu-dilneth-member-container">
+              <p className="chamindu-dilneth">Kishen Deemud</p>
+              <p className="member">Committee Member</p>
+            </b>
+          </div>
+          <div className="lead-pic-member2">
+            <div className="rectangle-parent1">
+              <div className="frame-child2" />
+              <img
+                className="member-icon1"
+                loading="eager"
+                alt=""
+                src={ThanutiPiyarathne}            />
+            </div>
+            <b className="dasith-sathpura-member-container">
+              <p className="dasith-sathpura">Thanuti Piyarathne</p>
+              <p className="member1">Committee Member</p>
+            </b>
+          </div>
+        </div>
 
 
 
